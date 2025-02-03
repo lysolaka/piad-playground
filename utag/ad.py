0a896f52280b1eca402d9ce9be8c8d2b5778da1b
lysolaka <lysolaka@gmail.com>
2025-02-03 10:53:43 +0100

    Live version of utag

--- utag/ad.py
+++ utag/ad.py
@@ -1,22 +1,21 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from pts_helper import get_pts
-
 import serial
 import tkinter as tk
 from tkinter import messagebox
+from pydwf import DwfLibrary, PyDwfError
+from pydwf.utilities import openDwfDevice
+from pydwf.core.api.protocol_uart import ProtocolUART
+
 
 SIG_END = b"\x04"
 SIG_ACK = b"\x06"
 SIG_REA = b"\x0e"
 SIG_WRT = b"\x0f"
+UART_TX = 8
+UART_RX = 0
 
 
 class Window:
-    def __init__(self, root: tk.Tk):
+    def __init__(self, root: tk.Tk, uart: ProtocolUART):
         self.root = root
         self.root.title("UART Tag")
 
@@ -38,8 +37,15 @@ class Window:
         self.read_bt = tk.Button(root, text="Read", command=self.read_tag)
         self.read_bt.grid(row=2, column=1, padx=10, pady=5)
 
-        self.ser = serial.Serial(get_pts("ad"), 9600, timeout=0)
-        self.ser.flush()
+        uart.reset()
+        uart.rateSet(9600.0)
+        uart.bitsSet(8)
+        uart.paritySet(0)
+        uart.stopSet(1)
+        uart.txSet(UART_TX)
+        uart.rxSet(UART_RX)
+        uart.rx(0)
+        self.uart = uart
 
     def update_id_box(self, text: str):
         self.id_box.config(state="normal")
@@ -51,11 +57,11 @@ class Window:
         self.update_id_box("ID is constant")
         content = self.text_box.get()
         messagebox.showinfo(title="Info", message="Place the tag on the sensor")
-        self.ser.write(SIG_WRT)
-        self.ser.write(content.encode())
-        self.ser.write(SIG_END)
+        self.uart.tx(SIG_WRT)
+        self.uart.tx(content.encode())
+        self.uart.tx(SIG_END)
         while True:
-            rx = self.ser.read(1)
+            rx, _ = self.uart.rx(1)
             if rx == SIG_ACK:
                 messagebox.showinfo(title="Info", message="Tag successfully written")
                 break
@@ -63,14 +69,14 @@ class Window:
     def read_tag(self):
         rx_buf = bytearray()
         messagebox.showinfo(title="Info", message="Place the tag on the sensor")
-        self.ser.write(SIG_REA)
+        self.uart.tx(SIG_REA)
         while True:
-            rx = self.ser.read(1)
+            rx, _ = self.uart.rx(1)
             if rx:
                 rx_buf += rx
                 if rx_buf[-1:] == bytearray(SIG_END):
                     break
-        self.ser.write(SIG_ACK)
+        self.uart.tx(SIG_ACK)
         rx_buf = rx_buf[:-1].decode()
         id, content = rx_buf.split(";", 1)
         messagebox.showinfo(title="Info", message="Tag successfully read")
@@ -79,6 +85,13 @@ class Window:
         self.text_box.insert(0, content)
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = Window(root)
-    root.mainloop()
+    try:
+        dwf = DwfLibrary()
+        with openDwfDevice(dwf) as device:
+            root = tk.Tk()
+            app = Window(root, device.protocol.uart)
+            root.mainloop()
+    except PyDwfError as exception:
+        print("PyDwfError:", exception)
+    except KeyboardInterrupt:
+        print("Keyboard interrupt, ending.")
